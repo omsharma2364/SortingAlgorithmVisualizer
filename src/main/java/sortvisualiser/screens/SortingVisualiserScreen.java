72fc90ccb7ad80ad2090afe25a432c30baf62fb7
omsharma2364 <omsharmadot2364@gmail.com>
2019-09-21 23:03:39 +0530

    Final commit

---
+++ src/main/java/sortvisualiser/screens/SortingVisualiserScreen.java
@@ -0,0 +1,77 @@
+package sortvisualiser.screens;
+
+import java.awt.BorderLayout;
+import java.util.ArrayList;
+
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.SwingWorker;
+import sortvisualiser.MainApp;
+import sortvisualiser.SortArray;
+import sortvisualiser.algorithms.ISortAlgorithm;
+public final class SortingVisualiserScreen extends Screen {
+    private final SortArray sortArray;
+    private final ArrayList<ISortAlgorithm> sortQueue;
+
+    /**
+     * Creates the GUI
+     * @param algorithms List of algorithms to run for visualisation
+     * @param app The main application
+     */
+    public SortingVisualiserScreen(ArrayList<ISortAlgorithm> algorithms, MainApp app) {
+        super(app);
+        setLayout(new BorderLayout());
+        sortArray = new SortArray();
+        add(sortArray, BorderLayout.CENTER);
+        sortQueue = algorithms;
+    }
+    
+    private void longSleep() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        } 
+    }
+
+    private void shuffleAndWait() {
+        sortArray.shuffle();
+        sortArray.resetColours();
+        longSleep();
+    }
+    
+    public void onOpen() {
+        //This would block the EventDispatchThread, and so
+        //it must run on a worker thread
+        SwingWorker<Void, Void> swingWorker = new SwingWorker<Void,Void>() {
+            @Override
+            protected Void doInBackground() throws Exception {
+                try {
+                    Thread.sleep(250);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                } 
+                for (ISortAlgorithm algorithm : sortQueue) {
+                    shuffleAndWait();
+                    
+                    sortArray.setName(algorithm.getName());
+                    sortArray.setAlgorithm(algorithm);
+        
+                    algorithm.runSort(sortArray);
+                    sortArray.resetColours();
+                    sortArray.highlightArray();
+                    sortArray.resetColours();
+                    longSleep();
+                }
+                return null;
+            }
+            
+            @Override
+            public void done() {
+                app.popScreen(); 
+            }
+        };
+        
+        swingWorker.execute();
+    }
+}
